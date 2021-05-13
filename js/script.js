7083624a0b05332745341bf90a1ab60901d70259
TK-Project <58058485+tk-project@users.noreply.github.com>
2021-05-14 01:36:23 +0900

    Update script.js

--- js/script.js
+++ js/script.js
@@ -128,9 +128,9 @@ $('[type="submit"]').on('click', function(){
 			var target = $($(this).attr('href')).offset().top;
 			target -= -70;
 			$('html, body').animate({ scrollTop: target }, 500);
-			return fa
+			return false;
 		});
 	});
 
 	
-});
+})(jQuery);
